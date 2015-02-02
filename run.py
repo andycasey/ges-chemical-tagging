b274aefc53d9a732e7b13f6c74c2f2a60bd30ceb
Andy Casey <andycasey@gmail.com>
2015-02-02 23:56:17 +0000

    runner.py

---
+++ run.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+""" Chemically tag the Gaia-ESO Survey data. """
+
+from __future__ import absolute_import, print_function, with_statement
+
+__author__ = "Andy Casey <arc@ast.cam.ac.uk>"
+
+
+# Module-specific.
+
+# PREPARATION
+# Create some data set.
+# Assign field stars based on rules.
+# Assign cluster candidates based on rules.
+# Assign cluster members based on rules.
+# Write the data to disk.
+
+# ASSIGNING REALISATIONS
+# Create realisations using the data table.
+# Save the information about the realisations to disk.
+
+# DATA ANALYSIS
+# For each realisation, we need to use particular amounts of data and try and
+# work out the number of clusters present.
+
+# Scripts for running on each realisation. Each script might be using a
+# different amount of data (e.g., metallicities only?)
