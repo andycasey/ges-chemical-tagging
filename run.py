2c3e18838e01cd64353bbc15a7e51728721c5b17
Andy Casey <andycasey@gmail.com>
2015-02-04 02:22:38 +0000

    a lot of tests planned...

--- run.py
+++ run.py
@@ -17,25 +17,94 @@ logger = logging.getLogger("code")
 # PREPARATION
 # Create some data set.
 
-# Assign field stars based on rules.
-# Assign cluster candidates based on rules.
-# Assign cluster members based on rules.
+# Remove *all* benchmarks!
+# Assign field stars based on rules (e.g., GES_MW_*)
+# Assign cluster candidates based on rules (by TARGET)
+# Assign cluster members based on rules (by TARGET + VELs)
 
 # Write the data to disk.
 #dataset.write("data/ges-data-set.fits")
 
 # ASSIGNING REALISATIONS
+# TEST 1 (TOY MODEL WITH POSITIONS AND? KINEMATICS)
+#---------------------------------------------------
+# Let's use the positional information (RA, DEC) to test the machinery. Set up
+# many permutations using a minimum number of stars in a cluster to be 5 (or 10;
+# whatever we need for it to work). Do this with GMM/AIC and VBGMM and DPGMM to
+# examine the differences in the method. Set some maximum limit on the max number
+# of stars in a cluster.
+
+
+# TEST 2 (METALLICITIES ONLY, AS A FUNCTION OF N_CLUSTERS)
+#----------------------------------------------------------
+# Chose different numbers of clusters to include in the set, and compare how
+# many that are recovered, using just [Fe/H]. Use the same (lower, upper) limits
+# on stars/cluster as in Test 1.
+
+
+# TEST 3 (METALLICITIES + ALPHAS)
+#---------------------------------
+# As per test 2, but use metallicities and alphas.
+# (consider doing with uncertainties as well)
+
+
+# TEST 4 (METALLICITIES + ALPHAS + Fe-peak)
+#-------------------------------------------
+# As per test 2, but use met + alpha + fe-p.
+# (considering doing by perturbing uncertainties as well)
+
+
+# TEST 5 (ALL ABUNDANCES)
+#--------------------------------------------
+# (perturb uncertainties as well)
+
+
+# TEST 6 (TAKE THE BEST COMBINATION OF ABUNDANCES, NO UNCERTAINTIES, ADD FIELD STARS)
+#-------------------------------------------------------------------------------------
+
+# TEST 7 (BEST COMBINATION OF ABUNDANCES, W/ UNCERTAINTIES, ADD FIELD STARS)
+#----------------------------------------------------------------------------
+
+# TEST 8 (MINIMUM NUMBER OF CLUSTER MEMBERS?)
+#---------------------------------------------
+# Is there a minimum number of stars per cluster? How do the above tests
+# change if we set the minimum stars in a cluster to be like 5, 10, 20, or 50?
+# How many clusters do we end up actually having?
+
+# TEST 9 (MINIMUM PRECISION REQUIRED FROM SOME TOY MODEL?)
+#----------------------------------------------------------
+# Take the mean abundance distributions of our clusters & approximate the covariance
+# between them. Then we could say "OK, some cluster might have these properties,
+# and the abundance differences within that cluster are immeasurable" -- and we
+# could ask the question "how well would we need to measure these abundances to
+# correctly infer their separation in amongst the field?"
+
+# TEST 9 is pretty much doing all the other tests using faux data, based on the
+# properties of the data. So we might need some code to create faux data based
+# on the properties of the real data....
+
+
+
 # Create realisations using the data table.
-# Save the information about the realisations to disk.
+# Save the realisations to disk for later.
 
 # DATA ANALYSIS
-# For each realisation, we need to use particular amounts of data and try and
-# work out the number of clusters present.
+# Create functions to run on a bunch of realisations, using some method, and
+# some columns of data >> save the results from each realisation to disk.
 
-# Scripts for running on each realisation. Each script might be using a
-# different amount of data (e.g., metallicities only?)
+# PLOTTING
+# Create way to plot hexbins/etc in some easy way.
+# Create way to plot discretised Ncluster_true along the x-axis too.
 
+# Make plots for all tests
 
+# OPEN QUESTIONS:
+# (1) Should any clusters be excluded because they are not really representative
+#     of the GALAH-like sample?
+# (2) How sensitive are the test results to the maximum number of stars in any
+#     given cluster?
+# (3) With the faux data, how do the tests scale to very large Ncluster?
+# (4) Do we need to really use WAIC?
 
 
 # SHOULD WE:
@@ -73,6 +142,7 @@ rs_indices, rs_counts, rs_hash = tagging.realisations.create(dataset,
     exclude_clusters=["Br25"], num_clusters=5)
 
 kwds = {
+    "full_output": True,
     "covariance_type": "full",
     "perturb_within_uncertainties": False
 }
